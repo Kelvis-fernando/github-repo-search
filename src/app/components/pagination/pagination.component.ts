5256a5dcc2c50883db0d40fd410d5ef2aed99346
Kelvis Delapena <kelvisdelapena@gmail.com>
2023-04-26 15:36:15 -0300

    chore; added the pagination component, and change the class name in card

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { GithubRepositoriesService } from './../../services/repositoriesService/github-repositories.service';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
-  styleUrls: ['./pagination.component.scss']
+  styleUrls: ['./pagination.component.scss'],
 })
 export class PaginationComponent {
+  @Input() currentPage: number = 1;
+  @Input() totalPages: number = 1;
+  @Output() pageChange: EventEmitter<number> = new EventEmitter();
 
+  constructor(private githubRepositoriesService: GithubRepositoriesService) {}
+
+  get pages(): number[] {
+    const pagesArray = [];
+    for (let i = 1; i <= this.totalPages; i++) {
+      pagesArray.push(i);
+    }
+    return pagesArray;
+  }
+
+  changePage(page: number) {
+    if (page > 0 && page <= this.totalPages) {
+      this.githubRepositoriesService
+        .getRepositories(page)
+        .subscribe((response: any) => {
+          this.currentPage = page;
+          this.pageChange.emit(page);
+        });
+      this.currentPage = page;
+      this.pageChange.emit(page);
+    }
+  }
 }
