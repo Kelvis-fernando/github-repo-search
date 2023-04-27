3b5d5d36b6e566115270a29c19048826055722d4
Kelvis Delapena <kelvisdelapena@gmail.com>
2023-04-27 11:06:32 -0300

    chore: added test to navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CardComponent } from './components/card/card.component';
 import { PaginationComponent } from './components/pagination/pagination.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { GithubRepositoriesService } from './services/repositoriesService/github-repositories.service';
+import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,7 @@ import { FormsModule } from '@angular/forms';
     PaginationComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
-  providers: [],
+  providers: [GithubRepositoriesService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
